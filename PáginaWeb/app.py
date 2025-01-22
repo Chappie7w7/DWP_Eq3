9fcc7e1e983775e8598da55b434da362c4dd692a
Chappie7w7 <josuelpzcruz11@gmail.com>
2025-01-22 15:44:13 -0600

    página de error

--- PáginaWeb/app.py
+++ PáginaWeb/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
@@ -32,10 +32,30 @@ def juegos():
 def proyectos():
     return render_template('josue/proyectos.html')
 
+# Ruta para manejar errores 400
+@app.errorhandler(400)
+def handle_bad_request(e):
+    return render_template('josue/error.html'), 400
+
+# Ruta para generar error 400
+@app.route('/suma')
+def suma():
+    # Verifica que los parámetros estén presentes
+    try:
+        a = int(request.args['a'])
+        b = int(request.args['b'])
+        return f"El resultado es: {a + b}"
+    except KeyError:
+        # Genera un error 400 si falta un parámetro
+        abort(400)
+    except ValueError:
+        # Genera un error 400 si los parámetros no son válidos
+        abort(400)
+
 # Ruta para manejar errores 404
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('josue/error.html'), 404
+#@app.errorhandler(404)
+#def page_not_found(error):
+#    return render_template('josue/error.html'), 404
 
 
 # Iniciar la aplicación
