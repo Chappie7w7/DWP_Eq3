c28fd1384338468a39b06ddb3c8423619fa406b9
Alexis1212asaxx <Felixalexissanchezlopez13@gmail.com>
2025-01-27 11:31:52 -0600

    login con validaciones

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from app.routes.auth_routes import auth_bp
-from app.routes.main import main_bp
+from flask_sqlalchemy import SQLAlchemy
+
+# Inicializar SQLAlchemy
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    # Configuración de la aplicación
+    app.config['SECRET_KEY'] = '4a15b75b799645eb3c35b2b4845418d2ff966c48294bbc4fc32758fd3cf0e239'  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # Ruta de la base de datos
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evitar advertencias innecesarias
+
+
+    db.init_app(app)
+
     # Registrar Blueprints
+    from app.routes.auth_routes import auth_bp
+    from app.routes.main import main_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(main_bp)
 
