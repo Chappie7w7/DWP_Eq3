dd1bb07b5cddbcf6fb54546820bb6dcbd4dbc856
Alexis1212asaxx <Felixalexissanchezlopez13@gmail.com>
2025-01-27 11:31:52 -0600

    login con validaciones

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
  #activar entorno virtual
   .\.venv\Scripts\activate   
 
-  #instalar requiriments
+  #instalar requirements
   pip install -r requirements.txt 
   python.exe -m pip install --upgrade pip  
 
@@ -16,3 +16,14 @@ Ejecutar
          O
   flask run
   
+  #GENERAR SECRET KEY
+   pwsh/python
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import secrets
+>>> print(secrets.token_hex(32))
+4a15b75b799645eb3c35b2b4845418d2ff966c48294bbc4fc32758fd3cf0e239
+
+
+ejecutar antes de iniciar la app
+python init_db.py
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,12 +1,34 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+
+from app.models.models import Usuario
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/logout')
 def logout():
-    # Aquí puedes agregar lógica para limpiar sesiones si es necesario
+    
     return redirect(url_for('auth.login'))
 
-@auth_bp.route('/login')
+# Expresión regular para validar correos
+email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.jinja')  # Asegúrate de que esta plantilla exista
+    if request.method == 'POST':
+        email = request.form.get('username')
+        password = request.form.get('password')
+
+        # Validar si los campos están vacíos
+        if not email or not password:
+            flash('Todos los campos son obligatorios', 'danger')
+            return render_template('auth/login.jinja')
+
+        # Verificar las credenciales del usuario
+        usuario = Usuario.query.filter_by(email=email).first()
+        if usuario and usuario.password == password:
+            return redirect(url_for('main.inicio'))
+        else:
+            flash('Usuario o contraseña incorrectos', 'danger')
+            return render_template('auth/login.jinja')
+
+    return render_template('auth/login.jinja')
