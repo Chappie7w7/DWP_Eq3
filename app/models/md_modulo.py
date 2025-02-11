748e26b8b9ee658c09db4be9b5f291cba03ca6dc
Alexis1212asaxx <Felixalexissanchezlopez13@gmail.com>
2025-02-11 00:08:56 -0600

    agregado tipos de busqueda

--- app/models/md_modulo.py
+++ app/models/md_modulo.py
@@ -5,11 +5,12 @@ class Modulo(db.Model):
     __table_args__ = {'mysql_charset': 'utf8mb4'}
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nombre = db.Column(db.String(100), unique=True, nullable=False)
+    nombre_modulo = db.Column(db.String(100), nullable=False)  
+    propietario = db.Column(db.String(100), nullable=False)  
 
     # Relaciones
     secciones = db.relationship('Seccion', back_populates='modulo', cascade='all, delete-orphan', lazy='joined')
     usuario_modulos = db.relationship('UsuarioModulo', back_populates='modulo', cascade='all, delete-orphan', lazy='joined')
 
     def __repr__(self):
-        return f'<Modulo {self.nombre}>'
+        return f'<Modulo {self.nombre_modulo} - {self.propietario}>'
